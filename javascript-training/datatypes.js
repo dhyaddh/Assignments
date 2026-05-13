1b9bd1a91711f2a8351c8684e6c8db87d5dbd588
dhyaddh2000 <dhyaddh2000@gmail.com>
2026-05-13 16:40:40 +0530

    Ok

---
+++ javascript-training/datatypes.js
@@ -0,0 +1,4 @@
+// Primitive data types which are Nimber , strings which are Immutabele
+// Non-Primitive Data which are OBJECTS , ARRAYS =They are Immutabele
+
+
---
+++ typescript-training/assignment1.ts
@@ -0,0 +1,40 @@
+// 1. Temperature of a city in degrees Celsius: 25.5
+let temparature: number = 25.5
+console.log(`Temparature of a city is: ${temparature} Celsius`)
+
+// 2. Whether a customer has placed an order: true or false
+
+let customerOrder: boolean = true
+console.log(`Customer order is placed: ${customerOrder}`)
+
+// 3. Person's phone number: "123-456-7890"
+let phoneNumber: string = "123-456-7890"
+console.log(`customer phone number is : ${phoneNumber}`)
+
+// 4. Amount of money in a customer's bank account: 1000.50
+let amount: number = 1000.50
+console.log(`Amount of money in a customer's bank account: ${amount}`)
+
+// 5. Person's email address: "john.doe@example.com"
+let email:string = "john.doe@example.com"
+console.log(`Person's email address is: ${email}`)
+
+// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates: (string|number)[] = ["latitude" , 37.7749, "longitude" , -122.4194]
+console.log(`Coordinates of a location: is ${coordinates}`)
+
+// 7. Person's marital status: true or false
+let marital: boolean = false
+console.log(`Person's Marital status: ${marital}`)
+
+// 8.Title of a book: "To Kill a Mockingbird"
+let title: string= "To Kill a Mockingbird"
+console.log(`Title of a book: ${title}`)
+
+// 16.Time of an event: 2:30 PM
+let eventDate: Date = new Date();
+
+eventDate.setHours(14);
+eventDate.setMinutes(30);
+
+console.log(`Time of an event: ${eventDate}`)
